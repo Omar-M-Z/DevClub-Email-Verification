4a9708af8446a89da8a4966220a62dbd80df2fbc
Omar <omar.m.zaheer@gmail.com>
2021-08-18 20:42:19 +0300

    added a function to check if emails entered are valid and started adding a system to give people a limited number of attempts for entering the proper information

--- Verification/EmailVerification.py
+++ Verification/EmailVerification.py
@@ -7,6 +7,7 @@ import os
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import requests
 
 def GetVerificactionCode():
     verificationCode = ""
@@ -15,7 +16,6 @@ def GetVerificactionCode():
             verificationCode = verificationCode + random.choice(string.ascii_letters)
         else:
             verificationCode = verificationCode + str(random.randint(0, 9))
-    print(verificationCode)
     return verificationCode
 
 def SendVerificationEmail(address, verificationCode):
@@ -31,7 +31,19 @@ def SendVerificationEmail(address, verificationCode):
         server.sendmail(os.environ.get("EMAIL_USER"), address, message.as_string())
         server.quit()
 
-usersInVerificationProcess = []
+def CheckEmail(email):
+    email_address = email
+    response = requests.get(
+        "https://isitarealemail.com/api/email/validate",
+        params={'email': email_address})
+    status = response.json()['status']
+    if status == "valid":
+        return True
+    else:
+        return False
+
+usersAndAttempts = {}
+verifiedRoleID = 99
 
 class EmailVerification(commands.Cog):
     def __init__(self, client):
@@ -41,27 +53,35 @@ class EmailVerification(commands.Cog):
     async def on_member_join(self, member):
         message = await member.send("To verify yourself and gain access to the server please enter your school email.")
         channel = message.channel
-        usersInVerificationProcess.append(member)
+        usersAndAttempts[member] = 3
+        verifcationCode = GetVerificactionCode()
 
         entry = None
         def check(entry):
-            return entry.content.endswith("isg.edu.sa") and entry.author == member and entry.channel == channel
+            if entry.content.endswith("isg.edu.sa") and entry.author == member and entry.channel == channel and CheckEmail(entry) == True:
+                SendVerificationEmail(entry.content, verifcationCode)
+                return True
+            else:
+                await member.send("That is not a valid email address.")
+                return False
         try:
             entry = await self.client.wait_for('message', check = check, timeout = 300)
             await member.send("You will be sent an email with a verification code. Please enter your verification code here.")
         except asyncio.TimeoutError:
-            await member.send("You have ran out of time.")
+            await member.send("You have ran out of time. Leave and rejoin the server to restart the verification process.")
+            usersAndAttempts.pop(member)
+            return
 
-        verifcationCode = GetVerificactionCode()
-        SendVerificationEmail(entry.content, verifcationCode)
         def check(entry):
             return entry.content == verifcationCode and entry.author == member and entry.channel == channel
         try:
             entry = await self.client.wait_for('message', check = check, timeout = 300)
             await member.send("You have successfully been verified and will gain access to the server.")
-            usersInVerificationProcess.remove(member)
+            usersAndAttempts.pop(member)
         except asyncio.TimeoutError:
-            await member.send("You have ran out of time.")
+            await member.send("You have ran out of time. Leave and rejoin the server to restart the verification process.")
+            usersAndAttempts.pop(member)
+            return
 
 def setup(client):
     client.add_cog(EmailVerification(client))
